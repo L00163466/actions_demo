2d49e28942d3be1eee24d49181b8fa1090e04c2b
L00163466 <91317437+L00163466@users.noreply.github.com>
2021-11-05 15:18:01 +0530

    Added file via upload

---
+++ src/main/java/LYITMvnTestApp/App.java
@@ -0,0 +1,13 @@
+/**
+ *
+ */
+package LYITMvnTestApp;
+/**
+ * @author Aravind.R
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello LYIT!");
+    }
+}
---
+++ src/test/java/LYITMvnTestApp/AppTest.java
@@ -0,0 +1,38 @@
+package LYITMvnTestApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+/**
+ * @author Aravind.R
+ *
+ */
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        final ByteArrayOutputStream outContent = new
+                ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        //make the output go to a stream instead of
+        //the console
+        App.main(null); //call main in the App file
+        //check if the output is "Hello LYIT!" and a new line.
+        assertEquals("Hello LYIT!" + System.getProperty("line.separator"), outContent.toString());
+    }
+
+    @Test
+    public void testMain() {
+        final ByteArrayOutputStream outContent = new
+                ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        //make the output go to a stream instead of
+        //the console
+        App.main(null); //call main in the App file
+        //check if the output is "Hello LYIT!" and a new line.
+        assertEquals("Hello LYIT" + System.getProperty("line.separator"), outContent.toString());
+    }
+}
