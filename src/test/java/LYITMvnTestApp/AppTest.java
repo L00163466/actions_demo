6a249c8eb4fd72005fca2e0ebaa8b3a035eea2dc
L00163466 <91317437+L00163466@users.noreply.github.com>
2021-11-05 15:33:31 +0530

    Removing the failure test case

--- src/test/java/LYITMvnTestApp/AppTest.java
+++ src/test/java/LYITMvnTestApp/AppTest.java
@@ -23,16 +23,4 @@ public class AppTest {
         //check if the output is "Hello LYIT!" and a new line.
         assertEquals("Hello LYIT!" + System.getProperty("line.separator"), outContent.toString());
     }
-
-    @Test
-    public void testMain1() {
-        final ByteArrayOutputStream outContent = new
-                ByteArrayOutputStream();
-        System.setOut(new PrintStream(outContent));
-        //make the output go to a stream instead of
-        //the console
-        App.main(null); //call main in the App file
-        //check if the output is "Hello LYIT!" and a new line.
-        assertEquals("Hello LYIT" + System.getProperty("line.separator"), outContent.toString());
-    }
 }
